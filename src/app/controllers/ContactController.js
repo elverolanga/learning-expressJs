175f73a7b718eff152230aa6c52d3ee75194b9ae
Martin Langa <167089903+elverolanga@users.noreply.github.com>
2025-07-21 13:24:56 +0200

    feat: added contact controller

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,172 @@
+import { Op } from 'sequelize';
+import { parseISO } from 'date-fns';
+import * as Yup from 'yup';
+import Contact from '../models/Contact.js';
+
+class ContactController {
+
+    async create(req, res){
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required,
+            status: Yup.string().uppercase(),
+            customer_id: Yup.number().required()
+        });
+
+        if(!(await schema.isValid(req.body)))
+        {
+            return res.status(400).json({error: "Invalid contact schema!"});
+        }
+
+        const contact = await Contact.create(req.body);
+        return res.status(201).json(contact);
+    }
+
+    async list(req, res){
+        const {
+            name,
+            email,
+            status,
+            createdBefore,
+            createdAfter,
+            updatedBefore,
+            updatedAfter,
+            sort,
+        } = req.query;
+
+        const page = req.params.page || 1;
+        const limit = req.params.limit || 25;
+
+        let where = {};
+        let order = [];
+
+        if(name)
+        {
+            where = {
+                ...where,
+                name: {
+                    [Op.like]: name,
+                },
+            }
+        }
+
+        if(email)
+        {
+            where = {
+                ...where,
+                email: {
+                    [Op.like]: email,
+                },
+            }
+        }
+
+        if(status)
+        {
+            where = {
+                ...where,
+                status: {
+                    [Op.in]: status.split(",").map(item => item.toUpperCase()),
+                },
+            }
+        }
+
+        if(createdBefore)
+        {
+            where = {
+                ...where,
+                createdBefore: {
+                    [Op.gte]: parseISO(createdBefore),
+                },
+            }
+        }
+        if(updatedBefore)
+        {
+            where = {
+                ...where,
+                updatedBefore: {
+                    [Op.gte]: parseISO(updatedBefore),
+                },
+            }
+        }
+
+        if(createdAfter)
+        {
+            where = {
+                ...where,
+                createdAfter: {
+                    [Op.gte]: parseISO(createdAfter),
+                },
+            }
+        }
+
+        if(updatedAfter)
+        {
+            where = {
+                ...where,
+                updatedAfter: {
+                    [Op.gte]: parseISO(updatedAfter),
+                },
+            }
+        }
+
+        if(sort)
+        {
+            order = sort.split(',').map(item => item.split(":"));
+        }
+
+        const data = await Contact.findAll(
+        {
+            where,
+            order,
+            limit,
+            offset: limit * page - limit,
+        });
+
+
+        return res.status(200).json(data);
+    }
+    async show(req, res){
+        const id = parseInt(req.params.id);
+        const data = await Contact.findByPk(id);
+        if(data){
+            return res.status(200).json(data);
+        }
+
+        return res.status(404).json('User not found!');
+    }
+    async update(req, res)
+    {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required,
+            status: Yup.string().uppercase(),
+            customer_id: Yup.number().required()
+        });
+
+        const data = await Contact.findByPk(req.params.id);
+        if(data)
+        {
+            return res.status(200).json(data);
+        }
+
+        if(!(await schema.isValid(req.body)))
+        {
+            return res.status(400).json('Invalid schema validation')
+        }
+
+        await data.update(res.body);
+        return res.status(200).json(data);
+    }
+
+    async delete(req, res){
+        const data = await Contact.findByPk(req.params.id);
+        if(!data){
+            return res.status(404).json({error: "Invalid Contact id"});
+        }
+
+        await data.destroy();
+        return res.status(200).json('Contact deleted successfully');
+    }
+}
+
+export default new ContactController();
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import customers from './app/controllers/CustomersController.js';
+import contacts from './app/controllers/ContactController.js'
 
 const routes = new Router();
 
@@ -8,5 +9,11 @@ routes.get("/customers/:id", customers.show);
 routes.post("/customers", customers.create);
 routes.put("/customers/:id", customers.update);
 routes.delete("/customers/:id", customers.destroy);
+
+routes.get("/contacts", contacts.list);
+routes.get("/contacts/:id", contacts.show);
+routes.post("/contacts", contacts.create);
+routes.put("/contacts/:id", contacts.update);
+routes.delete("/contacts/:id", contacts.delete);
 
 export default routes;
